6bdc92c408740e4ff12ba56972ee4d308519d74f
Aditya Shahi <ashahi1@ualberta.ca>
2025-01-06 17:34:02 -0700

    css finals added

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,8 @@
 import "@/styles/globals.css";
 import "@/styles/auth.css";
 import "@/styles/logogen.css";
-
+import "@/styles/loader.css";
+import "@/styles/starback.css";
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />;
 }
